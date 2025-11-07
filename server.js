18d8a730079e3e0c6d4b126a54d8765257cf7ae3
pablocreations <pablomr.5172@gmail.com>
2025-11-07 15:29:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("✅ Bot is alive");
-});
+app.get("/", (req, res) => res.send("✅ Bot is alive"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🌐 Web server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🌍 Web server running on port ${PORT}`));
